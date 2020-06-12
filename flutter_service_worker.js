8cf2ef09a96b13ba4c1ccb9132123c7c1562b43b
Vlad Rosca <58691756+VladRoscaDev@users.noreply.github.com>
2020-06-12 12:50:00 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "assets/AssetManifest.json": "7a4f9ee44294adbbc44788dfd3c34df8",
-"assets/assets/images/landing_page.jpg": "7c4bfc8adeae2423d624d31fcd357c30",
+"assets/assets/images/landing_page.jpg": "2c17383f6ab072fb79d0e5a1afd5a6a4",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "1a0088d67f81c3b3c237b1e4456d04ea",
@@ -12,7 +12,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2def70cdfb9efbc99017bc85e0c5e804",
 "/": "2def70cdfb9efbc99017bc85e0c5e804",
-"main.dart.js": "daa85c20cebc5b21ce590a73a14e51cf",
+"main.dart.js": "83aab7da2fee9ed050a014621b1c6490",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210"
 };
 
